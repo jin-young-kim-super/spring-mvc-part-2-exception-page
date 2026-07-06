537ae6a9e9aed10b257656a1f19df5f2a0fc2f52
Jin Young Kim <wlsdud66523@kakao.com>
2026-07-07 07:10:10 +0900

    API例外処理 - @ControllerAdvice

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -15,31 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiExceptionV2Controller {
 
-    // 이 컨트롤러 내에서 IllegalArgumentException 발생 시, 이 메서드에서 예외가 잡히고 API 오류 응답(ErroResult)가 API 오류 응답으로 반환된다.
-    // -> 물론 WAS의 재요청 없고, return new ModelAndVIew도 없고, response.getWriter()도 사용한 했고, 우리가 API 오류 응답을 커스터마이징 할 수가 있다.
-    // 물론 ExceptionResolver의 최우선 순위인 ExceptionHandlerExceptionResolver가 호출이 돼 처리한다.
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex",e);
-        return new ErrorResult("BAD",e.getMessage());
-    }
-
-    @ExceptionHandler(UserException.class)
-    public ResponseEntity<ErrorResult> useExHandler(UserException e) {
-        log.error("[exceptionHandler ] ex ", e);
-        ErrorResult errorResult = new ErrorResult("USR-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult,HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResult errorResult(Exception e) { // Exception : 예외의 최상위자
-        log.error("[exception ex]", e);           // -> 모든 예외의 최상위자이기에 예를 들어 자식 예외인 IllegalArugment, UserException 처리하는
-                                                  // @ExceptionHandler가 없으면 여기서 다 잡는다.
-        return new ErrorResult("Ex","내부 오류");
-    }
-
 
 
     // 만약 이때 런타임 에러가 터지면 HTML 오류 화면이 클라이언트에 제공되어 버린다.
---
+++ src/main/java/hello/exception/exHandler/advice/ExControllerAdvice.java
@@ -0,0 +1,37 @@
+package hello.exception.exHandler.advice;
+
+
+import hello.exception.exHandler.ErrorResult;
+import hello.exception.exception.UserException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex",e);
+        return new ErrorResult("BAD",e.getMessage());
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ErrorResult> useExHandler(UserException e) {
+        log.error("[exceptionHandler ] ex ", e);
+        ErrorResult errorResult = new ErrorResult("USR-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResult errorResult(Exception e) {
+        log.error("[exception ex]", e);
+        return new ErrorResult("Ex","내부 오류");
+    }
+}
